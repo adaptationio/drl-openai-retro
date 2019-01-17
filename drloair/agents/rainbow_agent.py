0682b3a94d8d1c594dc4ea3da2f28ca3c7d336fc
adaptationio <willie@adaptation.io>
2019-01-17 20:48:38 +1030

    refactor: ppo evalutate fix and rainbow agnet setup

--- drloair/agents/rainbow_agent.py
+++ drloair/agents/rainbow_agent.py
@@ -18,37 +18,55 @@ from ..agents.sonic_util import AllowBacktracking, make_env
 #from sonic_util import AllowBacktracking, make_env
 import retrowrapper
 import retro
+import csv
+import os
 
+
+from sonic_util import AllowBacktracking, make_env
 #env = retro.make(game='SonicTheHedgehog2-Genesis', state='MetropolisZone.Act1')
 class Rainbow():
     def __init__(self):
         self.love = 'Ramona'
+        self.env_fns = [] 
+        self.env_names = []
+        self.environs = ['SpringYardZone.Act3', 'SpringYardZone.Act2', 'GreenHillZone.Act3','GreenHillZone.Act1','StarLightZone.Act2','StarLightZone.Act1','MarbleZone.Act2','MarbleZone.Act1','MarbleZone.Act3','ScrapBrainZone.Act2','LabyrinthZone.Act2','LabyrinthZone.Act1','LabyrinthZone.Act3', 'SpringYardZone.Act1','GreenHillZone.Act2','StarLightZone.Act3','ScrapBrainZone.Act1']
+
+    def create_envs(self, n):
+            for i in self.environs:            
+                self.env_fns.append(partial(make_env, game='SonicTheHedgehog-Genesis', state=i, stack=False, scale_rew=False))
+                self.env_names.append('SonicTheHedgehog-Genesis' + '-' + i)
+
     def train(self):
         """Run DQN until the environment throws an exception."""
-        env = AllowBacktracking(make_env(stack=False, scale_rew=False))
-        env = BatchedFrameStack(BatchedGymEnv([[env]]), num_images=4, concat=False)
+        self.env_fns, self.env_names = self.create_envs() 
+        self.env = BatchedFrameStack(batched_gym_env(env_fns), num_images=4, concat=False)
         config = tf.ConfigProto()
         config.gpu_options.allow_growth = True # pylint: disable=E1101
         with tf.Session(config=config) as sess:
             dqn = DQN(*rainbow_models(sess,
-                                    env.action_space.n,
+                                    self.env.action_space.n,
                                     gym_space_vectorizer(env.observation_space),
                                     min_val=-421,
                                     max_val=421))
-            player = NStepPlayer(BatchedPlayer(env, dqn.online_net), 3)
+            player = NStepPlayer(BatchedPlayer(self.env, dqn.online_net), 3)
             optimize = dqn.optimize(learning_rate=1e-4)
             sess.run(tf.global_variables_initializer())
-            dqn.train(num_steps=2000000, # Make sure an exception arrives before we stop.
+            
+            dqn.train(num_steps=100000000, # Make sure an exception arrives before we stop.
                     player=player,
                     replay_buffer=PrioritizedReplayBuffer(500000, 0.5, 0.4, epsilon=0.1),
                     optimize_op=optimize,
                     train_interval=1,
                     target_interval=64,
                     batch_size=32,
-                    min_buffer_size=25000)
+                    min_buffer_size=25000,
+                    handle_ep=_handle_ep,
+                    num_envs = len(self.env_fns),
+                    save_interval=10)
 
-#if __name__ == '__main__':
-    #main()
+        
     
-    #print('exception', exc)
+                  )
+
+   
 
--- drloair/agents/stable_baseline_ppo.py
+++ drloair/agents/stable_baseline_ppo.py
@@ -57,31 +57,33 @@ class PPO2_SB():
         self.model.save(save)
     
     
-    def evaluate(self, num_steps=14400):
+    def evaluate(self, num_env=1, num_steps=14400):
         """
         Evaluate a RL agent
         :param model: (BaseRLModel object) the RL Agent
         :param num_steps: (int) number of timesteps to evaluate it
         :return: (float) Mean reward
         """
-        episode_rewards = [[0.0] for _ in range(env.num_envs)]
-        obs = env.reset()
+        self.create_envs(num_env)
+
+        episode_rewards = [[0.0] for _ in range(self.env.num_envs)]
+        obs = self.env.reset()
         for i in range(num_steps):
             # _states are only useful when using LSTM policies
             actions, _states = self.model.predict(obs)
             # # here, action, rewards and dones are arrays
             # # because we are using vectorized env
-            obs, rewards, dones, info = env.step(actions)
+            obs, rewards, dones, info = self.env.step(actions)
       
       # Stats
-            for i in range(env.num_envs):
+            for i in range(self.env.num_envs):
                 episode_rewards[i][-1] += rewards[i]
                 if dones[i]:
                     episode_rewards[i].append(0.0)
 
-        mean_rewards =  [0.0 for _ in range(env.num_envs)]
+        mean_rewards =  [0.0 for _ in range(self.env.num_envs)]
         n_episodes = 0
-        for i in range(env.num_envs):
+        for i in range(self.seenv.num_envs):
             mean_rewards[i] = np.mean(episode_rewards[i])     
             n_episodes += len(episode_rewards[i])   
 
